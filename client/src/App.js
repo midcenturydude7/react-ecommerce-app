2cf7ad1321cf5b4845e53b8c018e7c5598a0b1e3
midCenturyDude7 <mgriffes@gmail.com>
2023-02-28 18:02:13 -0800

    Initial commit | project structure/org

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.ScrollToTop(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
