9f61074185e26b6a21c023a4a0d3db39b02fb6c7
midCenturyDude7 <mgriffes@gmail.com>
2023-03-01 16:25:58 -0800

    Setting up Navbar component and state mngmt using Redux with toolkit

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Badge, Box, IconButton } from "@mui/material";
 import {
   PersonOutline,
-  ShoppingOutlined,
+  ShoppingBagOutlined,
   MenuOutlined,
   SearchOutlined,
 } from "@mui/icons-material";
@@ -25,7 +25,37 @@ const Navbar = () => {
       top="0"
       left="0"
       zIndex="1">
-      <Box></Box>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center">
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}>
+          ECOMMER
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2">
+          <IconButton sx={{ color: "black" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <PersonOutline />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <ShoppingBagOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
     </Box>
   );
 };
