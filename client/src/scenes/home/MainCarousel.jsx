b9a719cc389b7753004674fd0f3cb1b7c7f5d3bc
midCenturyDude7 <mgriffes@gmail.com>
2023-03-03 14:37:53 -0800

    Finished Item component and started Home subcomponents

---
+++ client/src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import
+
+const MainCarousel = () => {
+  return <div>Main Carousel</div>;
+};
+
+export default MainCarousel;
