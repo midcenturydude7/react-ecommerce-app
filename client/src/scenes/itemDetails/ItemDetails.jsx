8e6fa83834a9139b21349fb0d39d853458da0b0f
midCenturyDude7 <mgriffes@gmail.com>
2023-03-05 08:42:38 -0800

    Completed ItemDetails component

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { IconButton, Box, Typography, Button, Tabs, Tab } from "@mui/material";
-import { FavoriteBorderOutlinedIcon } from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import { shades } from "../../theme";
@@ -53,7 +53,7 @@ const ItemDetails = () => {
             alt={item?.name}
             width="100%"
             height="100%"
-            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            src={`http://localhost:2000${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
             style={{ objectFit: "contain" }}
           />
         </Box>
@@ -64,50 +64,80 @@ const ItemDetails = () => {
             <Box>Home/Item</Box>
             <Box>Prev Next</Box>
           </Box>
-          {/* COUNT & BUTTON */}
+
           <Box m="65px 0 25px 0">
             <Typography variant="h3">{item?.attributes?.name}</Typography>
-            <Typography>{item?.attributes?.price}</Typography>
+            <Typography>${item?.attributes?.price}</Typography>
             <Typography sx={{ mt: "20px" }}>
               {item?.attributes?.longDescription}
             </Typography>
-
-            <Box display="flex" alignItems="center" minHeight="50px">
-              <Box
-                display="flex"
-                alignItems="center"
-                border={`1.5px solid ${shades.neutral[300]}`}
-                mr="20px"
-                p="2px 5px">
-                <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
-                  <RemoveIcon />
-                </IconButton>
-                <Typography sx={{ p: "0 5px" }}>{count}</Typography>
-                <IconButton onClick={() => setCount(count + 1)}>
-                  <AddIcon />
-                </IconButton>
-              </Box>
-              <Button
-                sx={{
-                  backgroundColor: "#222222",
-                  color: "white",
-                  borderRadius: 0,
-                  minWidth: "150px",
-                  padding: "10px 40px",
-                }}
-                onClick={() =>
-                  dispatch(addToCart({ item: { ...item, count } }))
-                }>
-                ADD TO CART
-              </Button>
-            </Box>
-
-            <Box>
-              <Box m="20px 0 5px 0" display="flex">
-                <FavoriteBorderOutlinedIcon />
-              </Box>
-            </Box>
           </Box>
+
+          <Box display="flex" alignItems="center" minHeight="50px">
+            <Box
+              display="flex"
+              alignItems="center"
+              border={`1.5px solid ${shades.neutral[300]}`}
+              mr="20px"
+              p="2px 5px">
+              <IconButton onClick={() => setCount(Math.max(count - 1, 0))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography sx={{ p: "0 5px" }}>{count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+            <Button
+              sx={{
+                backgroundColor: "#222222",
+                color: "white",
+                borderRadius: 0,
+                minWidth: "150px",
+                padding: "10px 40px",
+              }}
+              onClick={() => dispatch(addToCart({ item: { ...item, count } }))}>
+              ADD TO CART
+            </Button>
+          </Box>
+          <Box>
+            <Box m="20px 0 5px 0" display="flex">
+              <FavoriteBorderOutlinedIcon />
+              <Typography sx={{ ml: "5px" }}>ADD TO WISHLIST</Typography>
+            </Box>
+            <Typography>CATEGORIES: {item?.attributes?.category}</Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* INFORMATION */}
+      <Box m="20px 0">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="DESCRIPTION" value="description" />
+          <Tab label="REVIEWS" value="reviews" />
+        </Tabs>
+      </Box>
+      <Box display="flex" flexWrap="wrap" gap="15px">
+        {value === "description" && (
+          <div>{item?.attributes?.longDescription}</div>
+        )}
+        {value === "reviews" && <div>reviews</div>}
+      </Box>
+
+      {/* RELATED ITEMS */}
+      <Box mt="50px" width="100%">
+        <Typography variant="h3" fontWeight="bold">
+          Related Products
+        </Typography>
+        <Box
+          mt="20px"
+          display="flex"
+          flexWrap="wrap"
+          columnGap="1.33%"
+          justifyContent="space-between">
+          {items.slice(0, 4).map((item, i) => (
+            <Item key={`${item.name}-${i}`} item={item} />
+          ))}
         </Box>
       </Box>
     </Box>
