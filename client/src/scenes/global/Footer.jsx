64c27efb287789fc97534d051931051354fcb66d
midCenturyDude7 <mgriffes@gmail.com>
2023-03-04 20:05:49 -0800

    Working on Footer component

---
+++ client/src/scenes/global/Footer.jsx
@@ -0,0 +1,24 @@
+import { useTheme } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { shades } from "../../theme";
+
+const Footer = () => {
+  const {
+    palette: { neutral },
+  } = useTheme();
+
+  return (
+    <Box mt="70px" p="40px 0" backgroundColor={neutral.light}>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px"></Box>
+    </Box>
+  );
+};
+
+export default Footer;
